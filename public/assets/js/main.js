5f1cb415350e7d9bb411f35c328ee77fd65c0d6b
Catherine Gu <catheg3@uci.edu>
2024-05-23 23:39:37 -0700

    Split Screen with invite buttons; When player leaves, remove all instances of player

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -17,7 +17,7 @@ function getIRIParameterValue(requestedKey) {
 let username = decodeURI(getIRIParameterValue('username'));
 
 if ((typeof username == 'undefined') || (username === null) || (username === 'null') || (username === '')) {
-    username = "Anonymous_" + Math.floor(Math.random()*1000);
+    username = "Anonymous_" + Math.floor(Math.random() * 1000);
 }
 
 /**Test to make sure name works 
@@ -31,9 +31,15 @@ if ((typeof chatRoom == 'undefined') || (chatRoom === null) || (chatRoom === 'nu
 
 /**Set up the socket.io connection to the server */
 let socket = io();
-socket.on('log', function(array) {
+socket.on('log', function (array) {
     console.log.apply(console, array);
 });
+
+function makeInviteButton() {
+    let newHTML = "<button type='button' class ='btn btn-outline-primary'>Invite</button>";
+    let newNode = $(newHTML);
+    return newNode;
+}
 
 socket.on('join_room_response', (payload) => {
     if ((typeof payload == 'undefined') || (payload === null)) {
@@ -44,7 +50,56 @@ socket.on('join_room_response', (payload) => {
         console.log(payload.message);
         return;
     }
-    let newHTML = '<p class=\'join_room_response\'>'+payload.username + ' joined the ' + payload.room + '. (There are ' + payload.count + ' users in this room)</p>';
+
+    /* If we are being notified of ourselves then ignore the message and return */
+    if (payload.socket_id === socket.id) {
+        return;
+    }
+
+    let domElements = $('.socket_' + payload.socket_id);
+    /* If we are being repeat notified then return */
+    if (domElements.length != 0) {
+        return;
+    }
+
+    /* <div class = "row align-items-center">
+            <div class = "col text-end">
+              Cat
+            </div>
+            <div class = "col text-end">
+               <button type = "button" class ="btn btn-primary">Invite<button>
+            </div>
+        </div>
+    */
+    let nodeA = $("<div></div>");
+    nodeA.addClass("row");
+    nodeA.addClass("align-items-center");
+    nodeA.addClass("socket_" + payload.socket_id);
+    nodeA.hide();
+
+    let nodeB = $("<div></div>");
+    nodeB.addClass("col");
+    nodeB.addClass("text-end");
+    nodeB.addClass("socket_" + payload.socket_id);
+    nodeB.append('<h4>' + payload.username + '</h4>');
+
+    let nodeC = $("<div></div>");
+    nodeC.addClass("col");
+    nodeC.addClass("text-start");
+    nodeC.addClass("socket_" + payload.socket_id);
+    let buttonC = makeInviteButton();
+    nodeC.append(buttonC);
+
+    /* Make them children */
+    nodeA.append(nodeB);
+    nodeA.append(nodeC);
+
+    $("#players").append(nodeA);
+    nodeA.show("fade", 1000);
+
+
+    /* Announcing in the chat that someone has arrived */
+    let newHTML = '<p class=\'join_room_response\'>' + payload.username + ' joined the ' + payload.room + '. (There are ' + payload.count + ' users in this room)</p>';
     let newNode = $(newHTML);
     newNode.hide();
     $('#messages').prepend(newNode);
@@ -56,7 +111,18 @@ socket.on('player_disconnected', (payload) => {
         console.log('Server did not send a payload');
         return;
     }
-    let newHTML = '<p class=\'left_room_response\'>'+payload.username + ' left the ' + payload.room + '. (There are ' + payload.count + ' users in this room)</p>';
+
+    /* remove everything attached to player that left */
+    if (payload.socket_id === socket.id) {
+        return;
+    }
+
+    let domElements = $('.socket_' + payload.socket_id);
+    if (domElements.length !== 0) {
+        domElements.hide("fade", 500);
+    }
+
+    let newHTML = '<p class=\'left_room_response\'>' + payload.username + ' left the ' + payload.room + '. (There are ' + payload.count + ' users in this room)</p>';
     let newNode = $(newHTML);
     newNode.hide();
     $('#messages').prepend(newNode);
@@ -82,7 +148,7 @@ socket.on('send_chat_message_response', (payload) => {
         console.log(payload.message);
         return;
     }
-    let newHTML = '<p class=\'chat_message\'><b>'+payload.username + '</b>: ' + payload.message + '</p>';
+    let newHTML = '<p class=\'chat_message\'><b>' + payload.username + '</b>: ' + payload.message + '</p>';
     let newNode = $(newHTML);
     newNode.hide();
     $('#messages').prepend(newNode);
@@ -99,13 +165,13 @@ $(() => {
 
     $('#lobbyTitle').html(username + "'s Lobby");
 
-    $('#chatMessage').keypress(function(e) {
+    $('#chatMessage').keypress(function (e) {
         let key = e.which;
         if (key == 13) { //the enter key is 13
-          $('button[id=chatButton]').click();
-          return false;
+            $('button[id=chatButton]').click();
+            return false;
         }
-      })
+    })
 });
 
 
