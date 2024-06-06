4fc95d6901e9e45fc42cc23e9683ab48b190dd5b
Catherine Gu <catheg3@uci.edu>
2024-06-06 12:07:26 -0700

    Added timer and final edits

--- server.js
+++ server.js
@@ -578,6 +578,10 @@ io.on('connection', (socket) => {
             game.legal_moves = calculate_legal_moves('w', game.board);
         }
 
+        /* set up when last legal game was made */
+        let d = new Date();
+        game.last_move_time = d.getTime();
+
         send_game_update(socket, game_id, 'played a token');
 
     });
